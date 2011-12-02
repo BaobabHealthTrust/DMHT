d6c707e9696c1192a676aace9b2077d52ba962e2
chawanangwa <chawanangwa.lupafya@baobabhealth.org>
2011-12-02 17:56:32 +0200

    change form input fields from 'custom' to  'touch screem form tags'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include AuthenticatedSystem
-	  Mastercard
+    Mastercard
     PatientIdentifierType
     PatientIdentifier
     PersonAttribute
