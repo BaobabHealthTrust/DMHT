1f28232708fdb3d8d50c95fb21e62062941ba741
blessings <blessings.mhango@gmail.com>
2012-11-14 15:53:48 +0200

    modified main next task method to check for diabetes initial questions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -957,149 +957,27 @@ class ApplicationController < GenericApplicationController
       return task.url if task.present? && task.url.present?
       return "/patients/show/#{patient.id}"
     rescue
-      return "/patients/show/#{patient.id}"
+      return "/patients/show/#{patient.id}" 
     end
   end
 
   # Try to find the next task for the patient at the given location
   def main_next_task(location, patient, session_date = Date.today)
+		task = Task.first rescue Task.new()
+		encounter_available = Encounter.find(:first,:conditions =>["patient_id = ? AND encounter_type = ? AND DATE(encounter_datetime) = ?",
+				                             patient.id,EncounterType.find_by_name("DIABETES INITIAL QUESTIONS").id, session_date],
+				                             :order =>'encounter_datetime DESC',:limit => 1)
+		task.encounter_type = 'DIABETES INITIAL QUESTIONS'
+		if encounter_available.blank?
+			task.url = "/encounters/new/diabetes_initial_questions?patient_id=#{patient.id}"
+		else
+			task.encounter_type = 'NONE'
+			task.url = "/patients/show/#{patient.id}"
+		end
 
-    if use_user_selected_activities
-      return next_form(location , patient , session_date)
-    end
-    all_tasks = Task.all(:order => 'sort_weight ASC')
-    todays_encounters = patient.encounters.find_by_date(session_date)
-    todays_encounter_types = todays_encounters.map{|e| e.type.name rescue ''}.uniq rescue []
-    all_tasks.each do |task|
-      next if todays_encounters.map{ | e | e.name }.include?(task.encounter_type)
-      # Is the task for this location?
-      next unless task.location.blank? || task.location == '*' || location.name.match(/#{task.location}/)
-
-      # Have we already run this task?
-      next if task.encounter_type.present? && todays_encounter_types.include?(task.encounter_type)
-
-      # By default, we don't want to skip this task
-      skip = false
-
-      # Skip this task if this is a gender specific task and the gender does not match?
-      # For example, if this is a female specific check and the patient is not female, we want to skip it
-      skip = true if task.gender.present? && patient.person.gender != task.gender
-
-      # Check for an observation made today with a specific value, skip this task unless that observation exists
-      # For example, if this task is the art_clinician task we want to skip it unless REFER TO CLINICIAN = yes
-      if task.has_obs_concept_id.present?
-        if (task.has_obs_scope.blank? || task.has_obs_scope == 'TODAY')
-          obs = Observation.first(:conditions => [
-          'encounter_id IN (?) AND concept_id = ? AND (value_coded = ? OR value_drug = ? OR value_datetime = ? OR value_numeric = ? OR value_text = ?)',
-          todays_encounters.map(&:encounter_id),
-          task.has_obs_concept_id,
-          task.has_obs_value_coded,
-          task.has_obs_value_drug,
-          task.has_obs_value_datetime,
-          task.has_obs_value_numeric,
-          task.has_obs_value_text])
-        end
-
-        # Only the most recent obs
-        # For example, if there are mutliple REFER TO CLINICIAN = yes, than only take the most recent one
-        if (task.has_obs_scope == 'RECENT')
-          o = patient.person.observations.recent(1).first(:conditions => ['encounter_id IN (?) AND concept_id =? AND DATE(obs_datetime)=?', todays_encounters.map(&:encounter_id), task.has_obs_concept_id,session_date])
-          obs = 0 if (!o.nil? && o.value_coded == task.has_obs_value_coded && o.value_drug == task.has_obs_value_drug &&
-            o.value_datetime == task.has_obs_value_datetime && o.value_numeric == task.has_obs_value_numeric &&
-            o.value_text == task.has_obs_value_text )
-        end
-
-        skip = true unless obs.present?
-      end
-
-      # Check for a particular current order type, skip this task unless the order exists
-      # For example, if this task is /dispensation/new we want to skip it if there is not already a drug order
-      if task.has_order_type_id.present?
-        skip = true unless Order.unfinished.first(:conditions => {:order_type_id => task.has_order_type_id}).present?
-      end
-
-      # Check for a particular program at this location, skip this task if the patient is not in the required program
-      # For example if this is the hiv_reception task, we want to skip it if the patient is not currently in the HIV PROGRAM
-      if task.has_program_id.present? && (task.has_program_workflow_state_id.blank? || task.has_program_workflow_state_id == '*')
-        patient_program = PatientProgram.current.first(:conditions => [
-          'patient_program.patient_id = ? AND patient_program.location_id = ? AND patient_program.program_id = ?',
-          patient.patient_id,
-          Location.current_health_center.location_id,
-          task.has_program_id])
-        skip = true unless patient_program.present?
-      end
-
-      # Check for a particular program state at this location, skip this task if the patient does not have the required program/state
-      # For example if this is the art_followup task, we want to skip it if the patient is not currently in the HIV PROGRAM with the state FOLLOWING
-      if task.has_program_id.present? && task.has_program_workflow_state_id.present?
-        patient_state = PatientState.current.first(:conditions => [
-          'patient_program.patient_id = ? AND patient_program.location_id = ? AND patient_program.program_id = ? AND patient_state.state = ?',
-          patient.patient_id,
-          Location.current_health_center.location_id,
-          task.has_program_id,
-          task.has_program_workflow_state_id], :include => :patient_program)
-        skip = true unless patient_state.present?
-      end
-
-      # Check for a particular relationship, skip this task if the patient does not have the relationship
-      # For example, if there is a CHW training update, skip this task if the person is not a CHW
-      if task.has_relationship_type_id.present?
-        skip = true unless patient.relationships.first(
-          :conditions => ['relationship.relationship = ?', task.has_relationship_type_id])
-      end
-
-      # Check for a particular identifier at this location
-      # For example, this patient can only get to the Pre-ART room if they already have a pre-ART number, otherwise they need to go back to registration
-      if task.has_identifier_type_id.present?
-        skip = true unless patient.patient_identifiers.first(
-          :conditions => ['patient_identifier.identifier_type = ? AND patient_identifier.location_id = ?', task.has_identifier_type_id, Location.current_health_center.location_id])
-      end
-
-      if task.has_encounter_type_today.present?
-        enc = nil
-        if todays_encounters.collect{|e|e.name}.include?(task.has_encounter_type_today)
-          enc = task.has_encounter_type_today
-        end
-        skip = true unless enc.present?
-      end
-
-      arv_drugs_given = false
-      PatientService.drug_given_before(patient,session_date).each do |order|
-        next unless MedicationService.arv(order.drug_order.drug)
-        arv_drugs_given = true
-      end
-
-      if task.encounter_type == 'ART ADHERENCE' and not arv_drugs_given
-        skip = true
-      end
-
-      if task.encounter_type == 'HIV CLINIC CONSULTATION' and (reason_for_art_eligibility(patient).blank? or reason_for_art_eligibility(patient).match(/unknown/i))
-        skip = true
-      end
-
-      if task.encounter_type == 'HIV STAGING' and not (reason_for_art_eligibility(patient).blank? or reason_for_art_eligibility(patient).match(/unknown/i))
-        skip = true
-      end
-
-      # Reverse the condition if the task wants the negative (for example, if the patient doesn't have a specific program yet, then run this task)
-      skip = !skip if task.skip_if_has == 1
-
-      # We need to skip this task for some reason
-      next if skip
-
-      if location.name.match(/HIV|ART/i) and not location.name.match(/Outpatient/i)
-       task = validate_task(patient,task,location,session_date.to_date)
-      end
-
-      # Nothing failed, this is the next task, lets replace any macros
-      task.url = task.url.gsub(/\{patient\}/, "#{patient.patient_id}")
-      task.url = task.url.gsub(/\{person\}/, "#{patient.person.person_id rescue nil}")
-      task.url = task.url.gsub(/\{location\}/, "#{location.location_id}")
-      Rails.logger.debug "next_task: #{task.id} - #{task.description}"
-
-      return task
-    end
+		return task
   end
+
 
 
   def validate_task(patient, task, location, session_date = Date.today)
